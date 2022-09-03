da5b6c8351545af0be7ce893af922c8f63d2e0bb
jnralexandre <alexandreoliveirajnr@gmail.com>
2022-09-02 21:57:17 -0300

    Adiciona exercicio01

---
+++ primeiro-modulo/logica-de-programacao/exercicios-com-scanner/src/br/com/exercicioscomscanner/Exercicio01.java
@@ -0,0 +1,20 @@
+package br.com.exercicioscomscanner;
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+    public static void main(String[] args) {
+        System.out.println("Informe a base do retângulo: ");
+        Scanner teclado = new Scanner(System.in);
+        double base = teclado.nextDouble();
+
+        System.out.println("Informe a altura do retângulo: ");
+        double altura = teclado.nextDouble();
+
+        double resultado = base * altura;
+
+        System.out.println("A área do triângulo é: " + resultado);
+    }
+
+}
