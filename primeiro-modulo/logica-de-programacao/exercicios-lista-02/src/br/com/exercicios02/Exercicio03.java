5d85762f87cebf7dbb7c87a14b5bd39285efcff5
jnralexandre <alexandreoliveirajnr@gmail.com>
2022-09-09 19:37:24 -0300

    Adiciona Exercicio03 exercicios lista 2

--- primeiro-modulo/logica-de-programacao/exercicios-lista-02/src/br/com/exercicios02/Exercicio03.java
+++ primeiro-modulo/logica-de-programacao/exercicios-lista-02/src/br/com/exercicios02/Exercicio03.java
@@ -1,4 +1,22 @@
 package br.com.exercicios02;
 
+import java.util.Scanner;
+
 public class Exercicio03 {
+
+    public static void main(String[] args) {
+        System.out.println("Informe o seu nome: ");
+        Scanner teclado = new Scanner(System.in);
+        String nomeCliente = teclado.nextLine();
+
+        System.out.println("Agora informe sua profissão: ");
+        String profissaoCliente = teclado.nextLine();
+
+        System.out.println("Por último informe seu salário: ");
+        double salarioCliente = teclado.nextDouble();
+
+        System.out.printf("Olá, seu nome é: %s; \nsua profissão é: %s; \ne o seu salário é de: RS " +
+                "%.2f ", nomeCliente, profissaoCliente, salarioCliente);
+    }
+
 }
