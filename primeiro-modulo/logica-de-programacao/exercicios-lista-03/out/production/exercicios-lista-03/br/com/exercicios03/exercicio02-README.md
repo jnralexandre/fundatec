c74983d06da74451da845d240ad32ea84e29c193
jnralexandre <alexandreoliveirajnr@gmail.com>
2022-09-09 22:03:00 -0300

    WIP: Adiciona Exercicio-03 exercicios lista 03

---
+++ primeiro-modulo/logica-de-programacao/exercicios-lista-03/out/production/exercicios-lista-03/br/com/exercicios03/exercicio02-README.md
@@ -0,0 +1,3 @@
+Uma moto percorre 240 km utilizando 20 litros de gasolina. Peça para o usuário
+informar a quantidade de km que ele deseja percorrer em uma viagem e calcula a
+quantidade de gasolina para realizar a viagem?
---
+++ primeiro-modulo/logica-de-programacao/exercicios-lista-03/src/br/com/exercicios03/Exercicio03.java
@@ -0,0 +1,27 @@
+package br.com.exercicios03;
+
+import java.util.Scanner;
+
+public class Exercicio03 {
+
+    public static void main(String[] args) {
+        System.out.println("Informe o número de votos válidos: ");
+        Scanner teclado = new Scanner(System.in);
+        int quantidadeVotosValidos = teclado.nextInt();
+
+        System.out.println("Agora informe o número de votos brancos: ");
+        int quantidadeVotosBrancos = teclado.nextInt();
+
+        System.out.println("Agora informe o número de votos nulos: ");
+        int quantidadeVotosNulos = teclado.nextInt();
+
+        int quantidadeTotalEleitores = quantidadeVotosValidos + quantidadeVotosBrancos + quantidadeVotosNulos;
+
+        double percentualVotosValidos = quantidadeVotosValidos - (quantidadeVotosBrancos + quantidadeVotosNulos);
+
+        double percentualVotosBrancos = quantidadeVotosBrancos - quantidadeVotosValidos + quantidadeVotosNulos;
+
+        System.out.printf("Número total de eleitores: %d; %nA porcentagem de votos válidos é: %.2f%s%nA porcentagem de votos brancos é: %.2f%s", quantidadeTotalEleitores, percentualVotosValidos, "%", percentualVotosBrancos, "%");
+    }
+
+}
