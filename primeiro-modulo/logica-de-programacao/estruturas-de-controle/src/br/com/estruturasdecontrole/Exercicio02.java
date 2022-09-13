9bc69455cf2bc0167ef06131ca101b667a96498e
jnralexandre <alexandreoliveirajnr@gmail.com>
2022-09-12 21:25:20 -0300

    Adiciona Exercicios estruturas de controle

---
+++ primeiro-modulo/logica-de-programacao/estruturas-de-controle/src/br/com/estruturasdecontrole/Exercicio02.java
@@ -0,0 +1,23 @@
+package br.com.estruturasdecontrole;
+
+import java.util.Scanner;
+
+public class Exercicio02 {
+
+    public static void main(String[] args) {
+        System.out.println("Informe um valor A: ");
+        Scanner teclado = new Scanner(System.in);
+        double valorA = teclado.nextDouble();
+
+        System.out.println("Informe um valor B: ");
+        double valorB = teclado.nextDouble();
+
+        if (valorA % valorB == 0) {
+            System.out.println("A é múltiplo de B.");
+        } else {
+            System.out.println("A não é múltiplo de B.");
+        }
+
+    }
+
+}
---
+++ primeiro-modulo/logica-de-programacao/estruturas-de-controle/src/br/com/estruturasdecontrole/exercicio02-README.md
@@ -0,0 +1,2 @@
+2) Elaborar um algoritmo que leia dois valores A e B e imprima: “A é múltiplo de B” ou
+“A não é múltiplo de B”.
