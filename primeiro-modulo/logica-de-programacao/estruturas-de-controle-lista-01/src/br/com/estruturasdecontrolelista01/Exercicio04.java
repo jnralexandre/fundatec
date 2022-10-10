21c836ce17c93b837b8a3700f236542b0175cd95
jnralexandre <alexandreoliveirajnr@gmail.com>
2022-10-10 16:14:59 -0300

    Adiciona Exercicio04

---
+++ primeiro-modulo/logica-de-programacao/estruturas-de-controle-lista-01/src/br/com/estruturasdecontrolelista01/Exercicio04.java
@@ -0,0 +1,32 @@
+package br.com.estruturasdecontrolelista01;
+
+import java.util.Scanner;
+
+public class Exercicio04 {
+
+    public static void main(String[] args) {
+        System.out.println("Por favor, informe seu salário: ");
+        Scanner teclado = new Scanner(System.in);
+        double salarioCliente = teclado.nextDouble();
+
+        System.out.println("Por fim, informe o valor total das suas vendas este mês: ");
+        double valorTotalVendas = teclado.nextDouble();
+
+        double salarioTotal = 0;
+
+        if (valorTotalVendas > 1000 && valorTotalVendas < 5000) {
+            double bonificacao = 200;
+            salarioTotal = salarioCliente + bonificacao;
+            System.out.printf("Seu salário total é: %.2f", salarioTotal);
+        } else if (valorTotalVendas > 5000 && valorTotalVendas < 10000) {
+            double bonificacao = 700;
+            salarioTotal = salarioCliente + bonificacao;
+            System.out.printf("Seu salário total é: %.2f", salarioTotal);
+        } else if (valorTotalVendas > 10000) {
+            double bonificacao = 1200;
+            salarioTotal = salarioCliente + bonificacao;
+            System.out.printf("Seu salário total é: %.2f", salarioTotal);
+        }
+    }
+
+}
