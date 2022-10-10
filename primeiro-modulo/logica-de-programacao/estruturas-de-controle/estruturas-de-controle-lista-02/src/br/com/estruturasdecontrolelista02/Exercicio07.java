d96929d7afcc7cb01b2b61502acc36aedb656101
jnralexandre <alexandreoliveirajnr@gmail.com>
2022-10-10 18:36:16 -0300

    Atualiza Exercicio07

---
+++ primeiro-modulo/logica-de-programacao/estruturas-de-controle/estruturas-de-controle-lista-02/src/br/com/estruturasdecontrolelista02/Exercicio07.java
@@ -0,0 +1,20 @@
+package br.com.estruturasdecontrolelista02;
+
+import java.util.Scanner;
+
+public class Exercicio07 {
+
+    public static void main(String[] args) {
+//        7. Desenvolva um algoritmo que efetue a leitura de um valor inteiro e informe se o
+//        mesmo é maior que 10.
+
+        System.out.println("Por favor, informe um número inteiro: ");
+        Scanner teclado = new Scanner(System.in);
+        int numeroInformado = teclado.nextInt();
+
+        if (numeroInformado > 10) {
+            System.out.println("O número informado é maior que 10.");
+        }
+    }
+
+}
