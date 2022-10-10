6d819b77acbc7cd2c40db17aa4755be7000d799a
jnralexandre <alexandreoliveirajnr@gmail.com>
2022-10-10 20:48:34 -0300

    Retira comentários

--- primeiro-modulo/logica-de-programacao/estruturas-de-repeticao/estruturas-de-repeticao-03/src/br/com/estruturasderepeticao03/Exercicio05.java
+++ primeiro-modulo/logica-de-programacao/estruturas-de-repeticao/estruturas-de-repeticao-03/src/br/com/estruturasderepeticao03/Exercicio05.java
@@ -3,10 +3,6 @@ package br.com.estruturasderepeticao03;
 import java.util.Scanner;
 
 public class Exercicio05 {
-
-//    5. Desenvolva um programa que receba o raio de um círculo enquanto o usuário não informar um valor negativo.
-//    Para cada valor lido o sistema deve efetuar o cálculo de sua área.
-
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
