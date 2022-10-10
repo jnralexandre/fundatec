e2618d787ff4062b7ae05981016715858ba9ac6a
jnralexandre <alexandreoliveirajnr@gmail.com>
2022-10-10 18:07:11 -0300

    Adiciona Exercicio02

---
+++ primeiro-modulo/logica-de-programacao/estruturas-de-controle/estruturas-de-controle-lista-02/src/br/com/estruturasdecontrolelista02/Exercicio02.java
@@ -0,0 +1,32 @@
+package br.com.estruturasdecontrolelista02;
+
+import java.util.Scanner;
+
+public class Exercicio02 {
+
+    public static void main(String[] args) {
+        System.out.println("Por favor, informe seu nome: ");
+        Scanner teclado = new Scanner(System.in);
+        String nomeCliente = teclado.nextLine();
+
+        System.out.println("Agora, informe sua primeira nota: ");
+        double primeiraNota = teclado.nextDouble();
+
+        System.out.println("Agora, informe sua segunda nota: ");
+        double segundaNota = teclado.nextDouble();
+
+        System.out.println("Agora, informe sua terceira nota: ");
+        double terceiraNota = teclado.nextDouble();
+
+        double mediaNotas = (primeiraNota + segundaNota + terceiraNota) / 3;
+
+        if (mediaNotas <= 5) {
+            System.out.printf("Olá %s, sua situação é: reprovado.%n", nomeCliente);
+        } else if (mediaNotas >= 5.1 && mediaNotas <= 6.9) {
+            System.out.printf("Olá %s, sua situação é: recuperação.%n", nomeCliente);
+        } else {
+            System.out.printf("Olá %s, sua situação é: aprovado.%n", nomeCliente);
+        }
+    }
+
+}
