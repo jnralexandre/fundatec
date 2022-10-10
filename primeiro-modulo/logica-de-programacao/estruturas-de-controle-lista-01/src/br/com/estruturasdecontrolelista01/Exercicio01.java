2bc304c757abe4cf95ad62f394da539d58d51fb6
jnralexandre <alexandreoliveirajnr@gmail.com>
2022-10-10 15:31:59 -0300

    Adiciona Exercicio01

---
+++ primeiro-modulo/logica-de-programacao/estruturas-de-controle-lista-01/src/br/com/estruturasdecontrolelista01/Exercicio01.java
@@ -0,0 +1,20 @@
+package br.com.estruturasdecontrolelista01;
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Por favor, informe um número inteiro: ");
+        int numeroInformado = teclado.nextInt();
+
+        if (numeroInformado % 3 == 0) {
+            System.out.printf("O número informado é múltiplo de 3: %d", numeroInformado);
+        } else {
+            System.out.printf("O número informado não é múltiplo de 3: %d", numeroInformado);
+        }
+    }
+
+}
