4e686e6b3a70dcf7613b77b333c689c1cb5cadfe
jnralexandre <alexandreoliveirajnr@gmail.com>
2022-10-11 18:15:58 -0300

    Adiciona Exercicios

---
+++ primeiro-modulo/logica-de-programacao/estruturas-de-repeticao/estruturas-de-repeticao-lista-02/src/br/com/entrega04/Exercicio06.java
@@ -0,0 +1,27 @@
+package br.com.entrega04;
+
+import java.util.Scanner;
+
+public class Exercicio06 {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        int numerosInformados = 0;
+        int somaNumerosPositivos = 0;
+        int quantidadeNumerosNegativos = 0;
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println("Por favor, informe um número inteiro");
+            numerosInformados = teclado.nextInt();
+            if (numerosInformados > 0) {
+                somaNumerosPositivos += numerosInformados;
+            } else if (numerosInformados < 0) {
+                quantidadeNumerosNegativos++;
+            }
+        }
+
+        System.out.printf("A soma dos números positivos é: %d%nA quantidade de números negativos é: %d", somaNumerosPositivos, quantidadeNumerosNegativos);
+    }
+
+}
---
+++ primeiro-modulo/logica-de-programacao/estruturas-de-repeticao/estruturas-de-repeticao-lista-02/src/br/com/entrega04/exercicio03-README.md
@@ -0,0 +1,1 @@
+3. Faça um algoritmo que mostre todos os múltiplos de 3 entre 1 e 50.
