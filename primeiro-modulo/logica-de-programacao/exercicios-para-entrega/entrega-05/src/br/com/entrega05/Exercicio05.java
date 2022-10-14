de028c7b741b55ab877ce6b3249a0bd8a4111a8c
jnralexandre <alexandreoliveirajnr@gmail.com>
2022-10-14 15:30:51 -0300

    Atualiza Exercicio05

--- primeiro-modulo/logica-de-programacao/exercicios-para-entrega/entrega-05/src/br/com/entrega05/Exercicio05.java
+++ primeiro-modulo/logica-de-programacao/exercicios-para-entrega/entrega-05/src/br/com/entrega05/Exercicio05.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Exercicio05 {
 
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-
         int totalIdades = 0;
         double totalPesos = 0;
+
+        Scanner teclado = new Scanner(System.in);
 
         for (int i = 0; i < 2; i++) {
             System.out.println("Por favor, informe seu peso: ");
@@ -23,9 +23,9 @@ public class Exercicio05 {
         }
 
         double mediaIdades = totalIdades / 2;
-        double mediaPeso = totalPesos / 2;
+        double mediaPesos = totalPesos / 2;
 
-        System.out.printf("A média da idade das pessoas é: %f%nO total das idades é: %d%nA média dos pesos é: %f%nO total dos pesos é: %f", mediaIdades, totalIdades, mediaPeso, totalPesos);
+        System.out.printf("A média da idade das pessoas é: %f%nO total das idades é: %d%nA média dos pesos é: %f%nO total dos pesos é: %f", mediaIdades, totalIdades, mediaPesos, totalPesos);
     }
 
 }
