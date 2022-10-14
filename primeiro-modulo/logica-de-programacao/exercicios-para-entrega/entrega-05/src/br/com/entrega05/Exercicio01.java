a70595938cd6aa7b1ce9725aadf056113f5ccfa9
jnralexandre <alexandreoliveirajnr@gmail.com>
2022-10-14 15:03:33 -0300

    Adiciona Exercicio01

---
+++ primeiro-modulo/logica-de-programacao/exercicios-para-entrega/entrega-05/src/br/com/entrega05/Exercicio01.java
@@ -0,0 +1,28 @@
+package br.com.entrega05;
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+    public static void main(String[] args) {
+        int numeroInformado = 0;
+        int somaPares = 0;
+        int somaImpares = 0;
+
+        Scanner teclado = new Scanner(System.in);
+
+        while (numeroInformado < 1000) {
+            System.out.println("Por favor, informe um número inteiro e positivo: ");
+            numeroInformado = teclado.nextInt();
+
+            if (numeroInformado % 2 == 0 && numeroInformado < 1001) {
+                somaPares += numeroInformado;
+            } else if (numeroInformado % 2 != 0 && numeroInformado < 1001) {
+                somaImpares += numeroInformado;
+            }
+        }
+
+        System.out.printf("A soma dos números pares é: %d%nA soma dos números ímpares é: %d%n", somaPares, somaImpares);
+    }
+
+}
