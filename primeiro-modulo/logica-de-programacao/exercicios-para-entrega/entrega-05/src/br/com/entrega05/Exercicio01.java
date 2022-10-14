c7a2a836efd38f5812965bd7a78901d22227e5f6
jnralexandre <alexandreoliveirajnr@gmail.com>
2022-10-14 15:33:21 -0300

    Atualiza codigo

--- primeiro-modulo/logica-de-programacao/exercicios-para-entrega/entrega-05/src/br/com/entrega05/Exercicio01.java
+++ primeiro-modulo/logica-de-programacao/exercicios-para-entrega/entrega-05/src/br/com/entrega05/Exercicio01.java
@@ -3,7 +3,6 @@ package br.com.entrega05;
 import java.util.Scanner;
 
 public class Exercicio01 {
-
     public static void main(String[] args) {
         int numeroInformado = 0;
         int somaPares = 0;
