e18a07c9acc3cfe65bfa04cbafdcfb1b901dc546
jnralexandre <alexandreoliveirajnr@gmail.com>
2022-11-07 20:07:57 -0300

    Adiciona exercicios

---
+++ primeiro-modulo/logica-de-programacao/manipulando-array/exercicios/src/br/com/exercicios/Exercicio02.java
@@ -0,0 +1,24 @@
+package br.com.exercicios;
+
+public class Exercicio02 {
+
+    public static void main(String[] args) {
+        int[] numeros = new int[10];
+
+        numeros[0] = 1;
+        numeros[1] = 2;
+        numeros[2] = 3;
+        numeros[3] = 4;
+        numeros[4] = 5;
+        numeros[5] = 6;
+        numeros[6] = 7;
+        numeros[7] = 8;
+        numeros[8] = 9;
+        numeros[9] = 10;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(numeros[i]);
+        }
+    }
+
+}
