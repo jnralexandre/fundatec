add7301ddf3c6efa32830fed5bec07416b63f098
jnralexandre <alexandreoliveirajnr@gmail.com>
2022-11-07 22:01:17 -0300

    Adiciona exercicios

---
+++ primeiro-modulo/logica-de-programacao/manipulando-array/exercicios-lista-01/src/br/com/exercicioslista01/Exercicio02.java
@@ -0,0 +1,25 @@
+package br.com.exercicioslista01;
+
+public class Exercicio02 {
+
+    public static void main(String[] args) {
+        double[] numerosInformados = new double[5];
+
+        numerosInformados[0] = 7.00;
+        numerosInformados[1] = -30.00;
+        numerosInformados[2] = -21.00;
+        numerosInformados[3] = 2.00;
+        numerosInformados[4] = -35.00;
+
+        int contador = 0;
+
+        for (int i = 0; i < 5; i++) {
+            if (numerosInformados[i] > 0) {
+                contador++;
+            }
+        }
+
+        System.out.printf("A quantidade de números positivos são: %d", contador);
+    }
+
+}
