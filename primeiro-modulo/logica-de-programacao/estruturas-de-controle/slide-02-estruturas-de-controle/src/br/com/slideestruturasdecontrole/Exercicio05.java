fe0593ea0fde4afae69a78596fb782d85cfa9492
jnralexandre <alexandreoliveirajnr@gmail.com>
2022-11-08 18:38:39 -0300

    Add exercises from slides 02

---
+++ primeiro-modulo/logica-de-programacao/estruturas-de-controle/slide-02-estruturas-de-controle/src/br/com/slideestruturasdecontrole/Exercicio05.java
@@ -0,0 +1,27 @@
+package br.com.slideestruturasdecontrole;
+
+import java.util.Scanner;
+
+public class Exercicio05 {
+
+    public static void main(String[] args) {
+        System.out.println("Informe o horário, com exceção dos minútos: ");
+        Scanner teclado = new Scanner(System.in);
+        int horarioInformado = teclado.nextInt();
+
+        System.out.println("Agora informe somente os minútos: ");
+        int minutosInformados = teclado.nextInt();
+
+
+        if (horarioInformado > 5 && horarioInformado < 13) {
+            System.out.println("Bom dia!");
+        } else if (horarioInformado >= 13 && horarioInformado <= 18) {
+            System.out.println("Boa tarde!");
+        } else if (horarioInformado >= 19 & horarioInformado <= 24 || horarioInformado == 00) {
+            System.out.println("Boa noite!");
+        } else {
+            System.out.println("O horário informado não está de acordo com o pedido.");
+        }
+    }
+
+}
